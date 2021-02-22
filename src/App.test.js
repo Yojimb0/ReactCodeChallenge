96352f126e9cbbe1543f5bc0b20994d2afaa98b4
Glitch (giant-psychedelic-bellflower) <none>
2021-02-22 13:21:44 +0000

    ⭐️🔑 Updated with Glitch

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import nock from 'nock';
+import { App } from './App';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+
+it('Shows error when 500 from API', () => {
+  const scope = nock('https://swapi.dev')
+  .get('/api/films/')
+  .reply(500);
+
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});*/
+
+
+import React from 'react';
+import nock from 'nock';
+import { render, screen } from '@testing-library/react';
+ 
+import {App} from './App';
+ 
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+  });
+  test('Shows error when 500 from API', () => {
+    const scope = nock('https://swapi.dev')
+      .get('/api/films/')
+      .reply(500);
+    
+    render(<App />);
+    
+    screen.getByText('Error occured.');
+  });
+});
+
---
+++ src/styled-components/Error.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const Error = styled.h2`
+  padding:60px 20px;
+  text-align:center;
+  color:red;
+`
+
+export default Error;
