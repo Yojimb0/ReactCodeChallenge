d57ca64c0d41d49927de3068c664e765a7b7653c
Glitch (giant-psychedelic-bellflower) <none>
2021-02-23 00:20:10 +0000

    🏆🔋 Updated with Glitch

--- src/App.test.js
+++ src/App.test.js
@@ -1,45 +1,24 @@
-/*import React from 'react';
-import ReactDOM from 'react-dom';
-import nock from 'nock';
-import { App } from './App';
-
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
-
-
-it('Shows error when 500 from API', () => {
-  const scope = nock('https://swapi.dev')
-  .get('/api/films/')
-  .reply(500);
-
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});*/
-
-
 import React from 'react';
 import nock from 'nock';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
  
 import {App} from './App';
  
 describe('App', () => {
+  
   test('renders App component', () => {
-    render(<App />);
+    const { container } = render(<App />);
+    expect(container).toMatchSnapshot();
   });
-  test('Shows error when 500 from API', () => {
+
+  test('Shows error when 500 from API', async () => {
+    let _body;
     const scope = nock('https://swapi.dev')
       .get('/api/films/')
       .reply(500);
     
     render(<App />);
-    
-    screen.getByText('Error occured.');
+    await waitFor(() => screen.getByText('Error occured.'))
   });
+  
 });
-
